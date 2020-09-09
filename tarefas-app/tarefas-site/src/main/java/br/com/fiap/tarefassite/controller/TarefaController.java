d14425b61b7d3221ccae9e3a5e6f3dc6194b41f9
Andrey Masiero <andreymasiero@gmail.com>
2020-09-09 10:53:05 -0300

    Finalizado o site da lista de aplicativos

--- tarefas-app/tarefas-site/src/main/java/br/com/fiap/tarefassite/controller/TarefaController.java
+++ tarefas-app/tarefas-site/src/main/java/br/com/fiap/tarefassite/controller/TarefaController.java
@@ -4,12 +4,16 @@ import br.com.fiap.tarefassite.domain.Tarefa;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/tarefas")
@@ -32,4 +36,51 @@ public class TarefaController {
                 tarefaResultado.getDescricao()));
         return "redirect:/tarefas";
     }
+
+    @GetMapping("/feita/{id}")
+    public String done(@PathVariable("id") long codigo, RedirectAttributes redirectAttributes) {
+        RestTemplate template = new RestTemplate();
+
+        Tarefa tarefa = template.getForObject("http://localhost:8084/tarefas/" + codigo, Tarefa.class);
+        tarefa.setFeita(!tarefa.getFeita());
+
+        if(tarefa.getDataLimite() == null || tarefa.getDataLimite().isBefore(LocalDate.now())) {
+            tarefa.setDataLimite(LocalDate.now());
+        }
+
+        Map<String, String> params = new HashMap<>();
+        params.put("id", String.valueOf(codigo));
+
+        template.put("http://localhost:8084/tarefas/{id}", tarefa, params);
+
+        redirectAttributes.addFlashAttribute("msg", tarefa.getFeita() ?
+                "Tarefa realizada com sucesso!" :
+                "Tarefa voltou para a lista.");
+        return "redirect:/tarefas";
+    }
+
+    @PostMapping("/excluir")
+    public String delete(long codigo, RedirectAttributes redirectAttributes) {
+        RestTemplate template = new RestTemplate();
+
+        Map<String, String> params = new HashMap<>();
+        params.put("id", String.valueOf(codigo));
+
+        template.delete("http://localhost:8084/tarefas/{id}", params);
+
+        redirectAttributes.addFlashAttribute("msg", "Tarefa excluída com sucesso!");
+        return "redirect:/tarefas";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
